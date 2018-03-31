6a1f8dcde6cb6fcd5a6364d39c33e3aaa1fa0d9b
abricker <abricker@wustl.edu>
2018-03-31 12:55:16 -0500

    Added new screens

---
+++ app/src/main/java/com/example/austinbricker/hackathon_project/Alarm.java
@@ -0,0 +1,11 @@
+package com.example.austinbricker.hackathon_project;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Alarm extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.alarm);
+//    }
+}
---
+++ app/src/main/java/com/example/austinbricker/hackathon_project/Voice.java
@@ -0,0 +1,15 @@
+package com.example.austinbricker.hackathon_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Voice extends AppCompatActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.voice);
+//    }
+
+
+}
